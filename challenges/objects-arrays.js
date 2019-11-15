457467a5a6560d6e40ea80f5a335a3f143c897c8
Zachary Mayfield <zakmayfield@gmail.com>
2019-11-15 11:45:02 -0500

    finished classes flie and stretch, added a class example to prototypes file

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -1,5 +1,7 @@
 // ==== Objects ====
 
+//NON CLASS VERSION
+
 /* 
   Given the following information about dinosaurs, create 3 objects: 
   Use this pattern to create your objects: 
@@ -56,6 +58,97 @@ console.log(tRex.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 console.log(tRex.roar());
+
+
+
+
+// CLASS VERSION
+
+/*
+class Jurassic{
+  constructor(attributes){
+    this.name = attributes.name;
+    this.diet = attributes.diet;
+    this.weight = attributes.weight;
+    this.length = attributes.length;
+    this.period = attributes.period;
+  }
+}
+
+class DinoRex extends Jurassic{
+  constructor(dinoAttrs){
+    super(dinoAttrs)
+  }
+  roar(){
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+}
+
+class DinoSteg extends Jurassic{
+  constructor(dinoAttrs){
+    super(dinoAttrs)
+  }
+}
+
+class DinoVelo extends Jurassic{
+  constructor(dinoAttrs){
+    super(dinoAttrs)
+  }
+}
+
+const tRex = new DinoRex({
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous'
+})
+
+const steggy = new DinoSteg({
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+})
+
+const velo = new DinoVelo({
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+})
+
+tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+
+stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+
+velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+Using your dinosaur objects, log answers to these questions:
+
+How much did tyrannosaurus weigh?
+console.log(tRex.weight);
+
+What was the diet of a velociraptor?
+console.log(velo.diet);
+
+How long was a stegosaurus?
+console.log(steggy.length);
+
+What time period did tyrannosaurus live in?
+console.log(tRex.period);
+
+
+Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+console.log(tRex.roar());
+*/
+
+
+
+
+
 
 
 // ==== Arrays ====
